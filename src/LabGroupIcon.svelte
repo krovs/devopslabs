0987c2a39a41d3a5b354721f4d83bb052f616e95
krovs <github@zvrok.com>
2026-06-02 22:52:04 +0200

    feat: update LabGroupIcon to include new icons and enhance menu group handling

--- src/LabGroupIcon.svelte
+++ src/LabGroupIcon.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
   import CertificateCheck from "carbon-icons-svelte/lib/CertificateCheck.svelte";
   import ChartLineData from "carbon-icons-svelte/lib/ChartLineData.svelte";
-  import CloudAuditing from "carbon-icons-svelte/lib/CloudAuditing.svelte";
   import Code from "carbon-icons-svelte/lib/Code.svelte";
-  import ContinuousDeployment from "carbon-icons-svelte/lib/ContinuousDeployment.svelte";
-  import Cost from "carbon-icons-svelte/lib/Cost.svelte";
   import DocumentTasks from "carbon-icons-svelte/lib/DocumentTasks.svelte";
   import Firewall from "carbon-icons-svelte/lib/Firewall.svelte";
   import FlowLogsVpc from "carbon-icons-svelte/lib/FlowLogsVpc.svelte";
   import FolderTree from "carbon-icons-svelte/lib/FolderTree.svelte";
   import GitRepo from "carbon-icons-svelte/lib/GitRepo.svelte";
   import GroupSecurity from "carbon-icons-svelte/lib/GroupSecurity.svelte";
+  import IbmCloudHyperProtectDbaas from "carbon-icons-svelte/lib/IbmCloudHyperProtectDbaas.svelte";
   import IbmSecurity from "carbon-icons-svelte/lib/IbmSecurity.svelte";
+  import LinuxAlt from "carbon-icons-svelte/lib/LinuxAlt.svelte";
+  import LogoKubernetes from "carbon-icons-svelte/lib/LogoKubernetes.svelte";
+  import Merge from "carbon-icons-svelte/lib/Merge.svelte";
+  import Network_3 from "carbon-icons-svelte/lib/Network_3.svelte";
+  import Pipelines from "carbon-icons-svelte/lib/Pipelines.svelte";
+  import PiggyBank from "carbon-icons-svelte/lib/PiggyBank.svelte";
   import type { MenuGroupId } from "./labCatalog";
 
   let { id, size = 32 }: { id: MenuGroupId; size?: number } = $props();
@@ -20,11 +24,15 @@
 {#if id === "terraform"}
   <Code {size} />
 {:else if id === "awsconfig"}
-  <CloudAuditing {size} />
+  <IbmCloudHyperProtectDbaas {size} />
 {:else if id === "cicd"}
-  <ContinuousDeployment {size} />
+  <Pipelines {size} />
 {:else if id === "gitops"}
   <GitRepo {size} />
+{:else if id === "linux"}
+  <LinuxAlt {size} />
+{:else if id === "kubernetes"}
+  <LogoKubernetes {size} />
 {:else if id === "terragrunt"}
   <FolderTree {size} />
 {:else if id === "iam"}
@@ -40,9 +48,11 @@
 {:else if id === "observability"}
   <ChartLineData {size} />
 {:else if id === "finops"}
-  <Cost {size} />
+  <PiggyBank {size} />
 {:else if id === "pr"}
-  <DocumentTasks {size} />
+  <Merge {size} />
+{:else if id === "networking"}
+  <Network_3 {size} />
 {:else}
   <Firewall {size} />
 {/if}
